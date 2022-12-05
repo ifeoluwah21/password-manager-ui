615146db6ff8659b3ccb6b39e304b05545494015
Ifeoulwah <spencerife.21@gmail.com>
2022-12-05 15:15:58 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Roboto Slab', "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+};
